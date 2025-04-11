3d8aa5b09f654b18ccaeb75e4bbd7dea4068c8a2
Desmond Nguyen <desmond.nguyen@laidon.com>
2025-04-11 17:13:41 +0700

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addTag('api') // Optional: gắn tag cho nhóm route
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // Swagger sẽ chạy tại /api
+
+  await app.listen(3000);
 }
 bootstrap();
