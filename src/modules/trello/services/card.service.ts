420ddfbe7b7f96f6c531128b0815c4752b232430
Desmond Nguyen <desmond.nguyen@laidon.com>
2025-06-10 10:33:52 +0700

    card

--- src/modules/trello/services/card.service.ts
+++ src/modules/trello/services/card.service.ts
@@ -1,18 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateCardDto } from '../dto/create-card.dto';
 import { Card } from '../entities/card.entity';
+import { List } from '../entities/list.entity';
 
 @Injectable()
 export class CardService {
   constructor(
     @InjectRepository(Card)
     private readonly cardRepo: Repository<Card>,
+    @InjectRepository(List)
+    private readonly listRepo: Repository<List>,
   ) {}
 
-  create(createCardDto: CreateCardDto) {
-    return this.cardRepo.save(createCardDto);
+  async create(createCardDto: CreateCardDto) {
+    const list = await this.listRepo.findOneBy({ id: createCardDto.listId });
+    if (!list)
+      throw new NotFoundException(`List ${createCardDto.listId} not found`);
+
+    const card = this.cardRepo.create({
+      ...createCardDto,
+      list,
+    });
+
+    return this.cardRepo.save(card);
   }
 
   findAll() {
